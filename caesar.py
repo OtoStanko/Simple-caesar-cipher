dd5e6737f1a3b93edf11bced80db745ec49f405c
Oto Stanko <493068@fi.muni.cz>
2022-11-28 21:30:12 +0100

    Initial commit

---
+++ caesar.py
@@ -0,0 +1,39 @@
+
+def getNewLetterUpper(letter, s):
+    return chr((ord(letter) + s - 65) % 26 + 65)
+
+
+def getNewLetterLower(letter, s):
+    return chr((ord(letter) + s - 97) % 26 + 97)
+
+
+def getComplementLetter(letter):
+    return chr((26 - (ord(letter) - 97)) % 26 + 97)
+
+
+def encrypt(text, word):
+    output = "";
+    s = [ord(w) - 97 for w in word]
+    k = len(word)
+    i = 0
+    while i < len(text):
+        letter = text[i]
+        if not letter.isalpha():
+            output += letter
+            i += 1
+            continue
+        if letter.isupper():
+            newLetter = getNewLetterUpper(letter, s[i%k])
+            output += newLetter
+        else:
+            newLetter = getNewLetterLower(letter, s[i%k])
+            output += newLetter
+        i += 1
+    return output
+
+
+def decrypt(text, word):
+    newWord = ""
+    for w in word:
+        newWord += getComplementLetter(w)
+    return encrypt(text, newWord)
